4e36e6536813e849dd17b5ee9d043f7d6abbf3ac
MrHeer <hlm52pk@163.com>
2021-02-05 16:56:04 +0800

    style: update card hover animation

--- src/app/project-card/project-card.component.ts
+++ src/app/project-card/project-card.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-project-card',
   templateUrl: './project-card.component.html',
-  styleUrls: ['./project-card.component.css'],
+  styleUrls: ['./project-card.component.scss'],
 })
 export class ProjectCardComponent implements OnInit {
   constructor() {}
